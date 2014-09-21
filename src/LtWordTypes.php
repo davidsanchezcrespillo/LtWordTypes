0edcff9d02fde710ce237159761e45795e93ce7a
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-21 21:26:09 +0200

    Introducing some test calls to the SQLite database

--- src/LtWordTypes.php
+++ src/LtWordTypes.php
@@ -1,6 +1,8 @@
 <?php 
 namespace LtWords\LtWordTypes;
- 
+
+use \PDO;
+
 /**
  * Class to identify word types in Lithuanian language.
  */
@@ -13,6 +15,38 @@ class LtWordTypes
    */
   public function getWordType($word)
   {
-      return "";
+    try {
+      //Connect to the database and open connections
+
+      $dbHandler = new PDO('sqlite:words.sqlite3');
+      // Set errormode to exceptions
+      $dbHandler->setAttribute(
+          PDO::ATTR_ERRMODE, 
+          PDO::ERRMODE_EXCEPTION
+      );
+ 
+      // Select all data from memory db messages table 
+      $statement = $dbHandler->prepare(
+          "SELECT * FROM words WHERE words.word = ?"
+      );
+
+      if ($statement->execute(array($word))) {
+          while ($row = $statement->fetch()) {
+              echo "Id: " . $row['id'] . "\n";
+              echo "Word: " . $row['word'] . "\n";
+              echo "Flags: " . $row['flags'] . "\n";
+              echo "\n";
+          }
+      }
+
+      //Close db connections
+      $dbHandler = null;
+    }
+    catch(PDOException $e) {
+      // Print PDOException message
+      echo $e->getMessage() . "\n";
+    }
+    
+    return "";
   }
 }
